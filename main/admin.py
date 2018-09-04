4058bc795856737311a7f7d328a47c854ab9f274
Spencer Sansone <spencersemail101@gmail.com>
2018-09-04 19:52:49 +0000

    add note model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+class NoteList(admin.ModelAdmin):
+    list_display = ('title',)
+    ordering = ['title']
+admin.site.register(Note, NoteList)
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=10000)
+    datetime_created = models.DateTimeField()
+
 # Create your models here.
