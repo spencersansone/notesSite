a00ea269b89de27c6b2e01945bcc37fe43585275
Spencer Sansone <spencersemail101@gmail.com>
2018-09-04 20:07:44 +0000

    add note category to models

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import *
 
+class NoteCategoryList(admin.ModelAdmin):
+    list_display = ('title',)
+    ordering = ['title']
+admin.site.register(NoteCategory, NoteCategoryList)
+
 class NoteList(admin.ModelAdmin):
     list_display = ('title',)
     ordering = ['title']
--- main/models.py
+++ main/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+class NoteCategory(models.Model):
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+
 class Note(models.Model):
     title = models.CharField(max_length=100)
+    category = models.ForeignKey(NoteCategory, on_delete=models.CASCADE)
     content = models.TextField(max_length=10000)
     datetime_created = models.DateTimeField()
 
